967697f11c4392b747820fda9be5f243250a17ae
Tommaso Fontana <zommy21@hotmail.it>
2019-01-10 12:40:35 +0100

    added braille symbols to the graph to print sub characters, so now it has x2 x res and x3 y res

--- curseswm/graph.py
+++ curseswm/graph.py
@@ -8,6 +8,18 @@ from .window import Window
 
 class Graph(Window):
 
+    symbols = {
+        (0,0): "⠉",
+        (0,1): "⠑",
+        (0,2): "⠡",
+        (1,0): "⠊",
+        (1,1): "⠒",
+        (1,2): "⠢",
+        (2,0): "⠌",
+        (2,1): "⠔",
+        (2,2): "⠤"
+    }
+
     def __init__(self,title : str = "", **kwargs):
         """Initialize the Graph.
         Graph(
@@ -36,6 +48,10 @@ class Graph(Window):
         """ resize(width : int, height : int) -> None
             Resize the graph to the new dimensions."""
         # pad the points to the new dim
+        super().resize(width,height)
+
+        # Calculate things using the double of width since braille symbols can rappresent 2 points
+        width = 2 * width
         if len(self.points) < width:
             # if the new width is bigger than the buffer then pad the head with zeros
             self.points = [0] * (width - len(self.points)) + self.points
@@ -43,7 +59,6 @@ class Graph(Window):
             # else select the readable data from the end
             self.points = self.points[-width:]
 
-        super().resize(width,height)
 
     def _print_central_line(self) -> None:
         with CentralLineColour(self.win):
@@ -80,20 +95,55 @@ class Graph(Window):
             self.minimum = self._momentum_update(self.minimum,new_min)
 
     def _draw_graph(self) -> None:
+        """draw on the screen the graph, this method check for the special cases"""
         with GraphColour(self.win):
-            delta = self.maximum - self.minimum
-            
+            delta = (self.maximum - self.minimum)
             if delta != 0:
-                coeff = (self.get_last_row() - 1) / delta
-
-                for x, point in enumerate(self.points[:self.width]):
-                    y = self.get_last_row() - int((point - self.minimum) * coeff)
-                    self.draw_text(x, y, self.draw_symbol)
-
+                self._print_graph(delta)
             else:
-                for x in range(self.width):
-                    self.draw_text(x, self.get_mid_row(), self.draw_symbol)
+                self._print_zero_line()
 
+    def _get_y_scaling_coeff(self, delta) -> float:
+        """Return the coefficient to transform a point to a y coordinate"""
+        # work with 3 times the y space since brailles symbols can print 3 rows
+        return 3 * (self.get_last_row() - 1) / delta
+
+    def _convert_point(self, point, coeff):
+        """Convert a point to a y coordinate"""
+        return (3*self.get_last_row()) - int((point - self.minimum) * coeff)
+
+    def _get_y_points(self, coeff)-> List[int]:
+        """Convert all the points to y coordinates"""
+        return [self._convert_point(point,coeff) for point in self.points]
+
+    def _get_symbol(self, y1, y2):
+        """Given the pair of points return the symbols that correspond to them"""
+        # If the values are in the same 3-chunk then assign them the related symbol
+        if abs(y1-y2) <= 3:
+            return self.symbols[(y1%3,y2%3)] 
+        # Else if the second point is in a lower chunk than the first one return a symbol with the second point on the lower row
+        elif y1 < y2:
+            return self.symbols[(y1%3,2)]
+        # Else if the second point is in a higher chunk than the first one return a symbol with the second point on the higher row
+        else:
+            return self.symbols[(y1%3,0)]
+
+    def _pair_of_coordinates(self, points):
+        """return a generator that returns pair of points in the form x, (y1, y2). the Iteration_0[y2] != Iteration_1[y1]"""
+        return enumerate(zip(points[::2],points[1::2]))
+
+    def _print_graph(self, delta):
+        """Print on the screen the actual graph"""
+        coeff = self._get_y_scaling_coeff(delta)
+        points = self._get_y_points(coeff)
+        for x, (y1, y2) in self._pair_of_coordinates(points):
+            self.draw_text(x, int(y1/3), self._get_symbol(y1,y2))
+
+
+    def _print_zero_line(self):
+        """Print a central line of dots, this is used when the points ar all the same and max == min."""        
+        for x in range(self.width):
+            self.draw_text(x, self.get_mid_row(), self.symbols[(1,1)])
 
     def _refresh_overriden(self) -> None:
         self._update_max_min()
